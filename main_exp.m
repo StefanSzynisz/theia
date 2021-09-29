e1dd5463ef42a398e67f0e0b654ff9284841cece
StefanSzynisz <stefan.t.szyniszewski@durham.ac.uk>
2021-09-30 00:22:01 +0100

    - added limits on -1.0 to 0.5 on Poisson ratio
    - added Moving average filter

--- main_exp.m
+++ main_exp.m
@@ -33,7 +33,7 @@ clear; tic;                                                                 % cl
 pressure = -0.1;                                                            % pressure in [Pa]
 element_size = 0.8592;                                                      % physical length (of pixel) in [m] or other consistent units
 bc_type = 'fixed';                                                          % type of boundaries at the bottom: 'fixed' or 'roller'
-
+  
 %% Read strain inputs:
 %****************************
 % readCSVfile
@@ -46,8 +46,8 @@ epsA = epsA(:,3:end);
 % **********************
 itMax = 18;                                                                 % maximum number of iterations
 tol   = 1e-7;                                                               % tolerance
-filter_type = 'None';                                                   % 'None','Gaussian' or 'MovingAverage'
-filter_size = 1;                                                            % 3, 5, or any higher odd number.
+filter_type = 'MovingAverage';                                              % 'None','Gaussian' or 'MovingAverage'
+filter_size = 3;                                                            % 3, 5, or any higher odd number.
 
 %% Set up the physical model (based on the user input and characteristics of the supplied data):
 % *************************************************************************
